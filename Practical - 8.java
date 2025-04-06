c3ade3edd6c39c97342ac0c6fe6e31fa1d63b084
Vegadabhagavat <162139105+Vegadabhagavat@users.noreply.github.com>
2025-04-06 14:20:59 +0530

    Update Practical - 8.java

--- Practical - 8.java
+++ Practical - 8.java
@@ -6,7 +6,7 @@ public class Car {
     }
   
     public static void main(String[] args) {
-        Car myCar = new Car(60);  // Creating a car object with speed of 60
+        Car myCar = new Car(60);  
         System.out.println("Car's speed = " + myCar.speed + " km/h");
     }
 }
